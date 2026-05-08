84e697460a1b5e6570b8ef22beb4e89976af80aa
kvniranwb <niranjanvt967@gmail.com>
2026-05-08 21:50:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let fname= document.getElementById("fname");
+let lname= document.getElementById("lname");
+let phone = document.getElementById("pnum");
+let email= document.getElementById("email");
+let int = document.getElementById("btn");
+console.log(fname)
+
+int.addEventListener("click",function(){
+alert("First Name :"+ fname.value+
+	   "\n Last Name :"+ lname.value+
+	    "\nPhone Number :"+ phone.value+
+	 "\n Email ID :" +email.value)})
